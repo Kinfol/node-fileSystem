eb03cfd84cd0cc987d35fc44c92b7cb79cacf221
Kinfol <Kinfol>
2019-11-28 16:47:43 +0000

    add test for available profuct

--- src/test/product.test.js
+++ src/test/product.test.js
@@ -1,7 +1,15 @@
 const p = require ('../product/product')
 
 describe("products", () => {
-	it("should show available products", () =>{
+	it("function exists", () =>{
 		expect(typeof p.product).toBe("function")
-		})
+        })
+        
+    it("should show available products", () =>{
+        console.log = jest.fn();
+        const cp = new p.product();
+        cp.availableProduct();
+        expect(console.log.mock.calls.length).toEqual(1);
+        expect(console.log.mock.calls[0][0]).toEqual("Product: Laptop, Brand: Dell, Price: 100£");
+    })
 })
