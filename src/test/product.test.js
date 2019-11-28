f63093b2a51c5a82b66c95fc32814dd045aab9aa
Kinfol <Kinfol>
2019-11-28 16:40:28 +0000

    test passing. function exists

--- src/test/product.test.js
+++ src/test/product.test.js
@@ -1,6 +1,5 @@
-const p = require ('./product.js')
+const p = require ('../product/product')
 
-// p.product = jest.fn();
 describe("products", () => {
 	it("should show available products", () =>{
 		expect(typeof p.product).toBe("function")
